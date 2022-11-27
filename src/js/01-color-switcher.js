984f3cd8fff98150ae8d9e2707d92a463722b6f7
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-11-27 15:19:51 +0200

    додає виконання першого завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-console.log('dddd')
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
+
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+   refs.startBtn.disabled = true;
+   refs.stopBtn.disabled = false;
+   intervalId = setInterval(() => {
+   document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function onStopBtnClick() {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+    clearInterval(intervalId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
