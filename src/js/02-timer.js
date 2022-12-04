0e5aee03a2be3052f350eef7e5f65f134f0877f9
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-12-04 20:06:08 +0200

    додає виконання другого і третього завдання

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,80 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const element = document.querySelector('input#datetime-picker');
+let selectedDate;
+let intervalId = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentTime = Date.now();
+    validDate = selectedDates[0] - currentTime;
+    if (validDate < 0) {
+      alert('Please choose a date in the future');
+      return;
+    }
+    refs.stratBtn.disabled = false;
+    selectedDate = selectedDates[0];
   },
 };
 
-const element = document.querySelector('input#datetime-picker');
-
 flatpickr(element, options);
+
+const refs = {
+  stratBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]')
+}
+
+
+
+refs.stratBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+    intervalId = setInterval(() => {
+    const clickCurrentTime = Date.now();
+    const ms = selectedDate - clickCurrentTime;
+    const time = convertMs(ms);
+    updateTexContent(time);
+    if (ms < 1000) {
+    clearInterval(intervalId);
+      };
+    }, 1000);
+}
+
+function updateTexContent({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {
+  form: document.querySelector('form')
+}
+
+let delay = 0;
+let step = 0;
+let amount = 0;
+let position = 0;
+
+refs.form.addEventListener('submit', onSubmitBtnClick);
+
+function onSubmitBtnClick(event) {
+  event.preventDefault();
+  delay = Number(event.currentTarget.delay.value);
+  step = Number(event.currentTarget.step.value);
+  amount = Number(event.currentTarget.amount.value);
+  makePromises();
+  event.currentTarget.reset();
+  position = 0;
+}
+
+function makePromises() {
+  for (let i = 0; i < amount; i += 1) {
+    if (i === 0 ) {
+   position += 1;
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });  
+    } else {
+    delay += step;
+    position += 1;
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });  
+    }
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+      resolve({position, delay});
+    } else {
+      reject({position, delay});
+    } 
+    }, delay); 
+    });
 }
